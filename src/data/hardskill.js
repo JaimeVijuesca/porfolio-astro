b78b71970ee63968fcd9f920587bb8009df7f8af
Jaime Vijuesca <jaimevijuescafraca@gmail.com>
2025-11-26 16:41:47 +0100

    change the path hardskill

--- src/data/hardskill.js
+++ src/data/hardskill.js
@@ -2,42 +2,42 @@ export const hardSkills = [
     {
         id: 1,
         name: "JavaScript",
-        icon: "src/assets/icons/javascript.svg"
+        icon: "/assets/icons/javascript.svg"
     },
     {
         id: 3,
         name: "Java",
-        icon: "src/assets/icons/java.svg"
+        icon: "/assets/icons/java.svg"
     },
     {
         id: 4,
         name: ".NET",
-        icon: "src/assets/icons/dotnet.svg"
+        icon: "/assets/icons/dotnet.svg"
     },
     {
         id: 5,
         name: "HTML5",
-        icon: "src/assets/icons/html5.svg"
+        icon: "/assets/icons/html5.svg"
     },
     {
         id: 6,
         name: "CSS3",
-        icon: "src/assets/icons/css3.svg"
+        icon: "/assets/icons/css3.svg"
     },
     {
         id: 7,
         name: "Tailwind CSS",
-        icon: "src/assets/icons/tailwindcss.svg"
+        icon: "/assets/icons/tailwindcss.svg"
     },
     {
         id: 8,
         name: "Astro",
-        icon: "src/assets/icons/astro.svg"
+        icon: "/assets/icons/astro.svg"
     },
     {
         id: 9,
         name: "Git",
-        icon: "src/assets/icons/git.svg"
+        icon: "/assets/icons/git.svg"
     }
 ];
 
